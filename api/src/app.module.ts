5b6b16c0a1c01bfae90cbfec39b28c32bb51b2ff
Drew Kimberly <drew.kimberly@konghq.com>
2023-01-09 16:11:37 -0600

    feat(api): add health api

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,13 +1,10 @@
+import { HealthApiModule } from './health/api/health.api.module';
 import { UserModule } from './user/lib/user.module';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { DatabaseModule } from './database';
 
 import { Module } from '@nestjs/common';
 
 @Module({
-  imports: [DatabaseModule, UserModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [DatabaseModule, HealthApiModule, UserModule],
 })
 export class AppModule {}
