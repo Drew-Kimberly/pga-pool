1ec460ae9c10de4b7066a4bcfa9c2e2ae435e701
Drew Kimberly <drew.kimberly@konghq.com>
2023-01-15 15:27:43 -0600

    feat(api): add CORS headers

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,6 +4,10 @@ import { NestFactory } from '@nestjs/core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
